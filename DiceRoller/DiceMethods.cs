4596334f5fd3960ef4286ebf6b6888e8a20f8498
Chris Schmalz <cschmalz03@gmail.com>
2023-01-22 22:43:30 -0500

    initial commit

---
+++ DiceRoller/DiceMethods.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    internal class DiceMethods
+    {
+        public static int ReturnRandomNumber(int sides)
+        {
+            var ran = new Random();
+            int ranResult = ran.Next(1, sides + 1);
+            return ranResult;
+        }
+        public static string ReturnValidCombination(int die1, int die2)
+        {
+            string nickName = string.Empty;
+            int dieTotal = die1 + die2;
+            if (dieTotal == 2)
+            {
+                nickName = "Snake Eyes!";
+            }
+            else if (dieTotal == 3)
+            {
+                nickName = "Ace Deuce!";
+            }
+            else if (dieTotal == 6) 
+            {
+                nickName = "Box Cars!";
+            }
+            else
+            {
+            }
+            return nickName;
+        }
+        public static string ReturnValidCombination2(int die1, int die2)
+        {
+            string nickName = string.Empty;
+            int dieTotal = die1 + die2;
+            if (dieTotal == 7 || dieTotal == 11)
+            {
+                nickName = "Win!";
+            }
+            else if (dieTotal == 2 || dieTotal == 3 || dieTotal == 12)
+            {
+                nickName = "Craps!";
+            }
+            else
+            {
+            }
+            return nickName;
+        }
+    }
+}
---
+++ DiceRoller/Program.cs
@@ -0,0 +1,63 @@
+﻿using DiceRoller;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+
+bool playagain = true;
+do
+{
+    Console.WriteLine("Rolling a 6 sided pair of dice");
+    int num = 6;
+
+    Console.WriteLine("Ready to roll the dice? Press enter to continue, q to quit");
+    string dicedecision = Console.ReadKey().Key.ToString();
+
+    if (dicedecision == "Enter")
+    {
+        int firstDie = DiceMethods.ReturnRandomNumber(num);
+        Console.WriteLine($"Your first dice rolled a {firstDie}");
+
+        int secondDie = DiceMethods.ReturnRandomNumber(num);
+        Console.WriteLine($"Your second dice rolled a {secondDie}");
+
+        int dieTotal = firstDie + secondDie;
+
+        Console.WriteLine($"That's for a total of {dieTotal}");
+
+        string nickName = DiceMethods.ReturnValidCombination(firstDie, secondDie);
+        if (nickName != "")
+        {
+            Console.WriteLine(nickName);
+        }
+
+        string nickName2 = DiceMethods.ReturnValidCombination2(firstDie, secondDie);
+        if (nickName2 != "")
+        {
+            Console.WriteLine(nickName2);
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("Roll again? (y/n)");
+
+        string answer = Console.ReadLine();
+
+        if (answer == "y")
+        {
+            playagain = true;
+        }
+        else if (answer == "n")
+        {
+            Console.WriteLine("Goodbye!");
+            playagain = false;
+        }
+
+        Console.WriteLine();
+    }
+    else if (dicedecision.ToUpper() == "Q")
+    {
+        playagain = false;
+        Console.WriteLine();
+        Console.WriteLine("Goodbye!");
+    }
+}
+while (playagain);
